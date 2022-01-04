dffa1f9c698f7f6e2de879d0d367e7a33accc146
Bhushan <bhushangaykawad@gmail.com>
2022-01-04 18:08:50 +0530

    refined grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,49 @@ import React from 'react';
 import Node from './components/Node';
 import './App.css'
 
-const NODE_SIZE = 408;
+const BOARD_COL = 24;
+const BOARD_ROW = 16;
+
+const START_NODE_COL = 2;
+const START_NODE_ROW = 5;
+
+const FINISH_NODE_COL = 22;
+const FINISH_NODE_ROW = 14;
+
+const createNode = (col: number, row: number) => {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+  };
+};
 
 const range = (max: number) => Array.from(Array(max).keys())
+
+const grid = () => {
+  return range(BOARD_ROW).map(row => {
+    return range(BOARD_COL).map(col => {
+      return createNode(col, row);
+    });
+  });
+}
 
 const App = () => {
   return (
     <div className="container">
       <div className="grid">
         {
-          range(NODE_SIZE).map(item => {
-            return <Node
-              key={item}
-              value={item}
-            />
+          grid().map(x => {
+            return x.map(y => {
+              return <Node
+                key={`${y.col}-${y.row}`}
+                col={y.col}
+                row={y.row}
+                isStart={y.isStart}
+                isFinish={y.isFinish}
+              />;
+            })
           })
         }
       </div>
--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,5 +1,10 @@
-const Node = (props: { value: number }) => {
-  return <div className="node">{props.value + 1}</div>
+import './Node.css'
+
+const Node = (props: { col: number, row: number, isStart: boolean, isFinish: boolean }) => {
+  const startClass = props.isStart ? 'node-start' : '';
+  const finishClass = props.isFinish ? 'node-end' : '';
+
+  return <div className={`node ` + startClass + finishClass} />
 }
 
 export default Node;
